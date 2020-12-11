fc4804de57382c96919aac57d768bdd64d4d48b9
fredrmag <fredrma@gmail.com>
2020-12-11 12:53:23 +0000

    Add __init__.py file, as well as including the report.tpl file to package installation.

--- setup.py
+++ setup.py
@@ -7,12 +7,13 @@ setuptools.setup(
     name="python_dmri_preprocessing-fredrmag", # Replace with your own username
     version="0.1.0",
     author="Fredrik Magnussen",
-    author_email="fredrik.magnussen@psykologi.uio.no.com",
+    author_email="fredrik.magnussen@psykologi.uio.no",
     description="Preprocessing of dMRI data",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/LCBC-UiO/python_dmri_preprocessing",
     packages=setuptools.find_packages(),
+    package_data={'dmri_preprocessing': ['report/report.tpl']},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
